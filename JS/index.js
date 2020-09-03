30be729bc1481094befcc8763f6c886d74edc749
alex rothenburger <alex.rothenburger@gmail.com>
2020-09-03 15:29:33 +0200

    index reformat OK

--- JS/index.js
+++ JS/index.js
@@ -1,64 +1,47 @@
-//----------------------------------URL PRODUIT DE L'API
+//---------------------URL PRODUIT DE L'API
 const url = "http://localhost:3000/api/cameras/";
 
-function numberWithCommas(x) {
-    return x.toString().replace(/d{2}/, ",");
-}
-console.log(numberWithCommas(59000))
+//----------------------BLOC PARENT
+let container = document.querySelector('main');
+container.className = "container-fluid row mx-auto"
 
-//----------------------------------RECUPERATION DES PRODUITS
+          
+//---------------------_SI LA REQUETE A ABOUTI...
+fetchIndex(url).then(itemsData =>{
+    for (let i in itemsData) { //...GENERER LES BLOCS
+        createItemBloc(itemsData[i]);}
+    })
+    .catch(error =>{ //...SINON AFFICHER :
+         container.innerHTML = `<h2 class="h4 text-center col-12 mt-5 pt-4">
+         Une erreur est survenue, veuillez recharger la page.</h2>`   
+    });
+
+//---------------------FONCTION : RETOURNER LE CONTENU DE LA REQUETE
 async function fetchIndex() {
 
-    try{
-
-    let response = await fetch(url); // REQUETE
+    let response = await fetch(url); 
     if (response.ok) {
-        let itemsData = await response.json(); // REPONSE PARSÉE
+        let itemsData = await response.json();
+        return itemsData; 
+    };
+};      
+    
+//----------------------FONCTION : CREER LE BLOC ITEM
+function createItemBloc(item) {
 
-//----------------------------------- TEMPLATE
-        for (let i in itemsData) {
-
-            let mainBloc = document.querySelector('main')
-            mainBloc.className ="container-fluid row mx-auto";
-
-            let itemsLink = document.createElement('a');
-            itemsLink.className = "badge badge-light col-md-6 col-sm-12 mx-auto "
-            itemsLink.style.whiteSpace ="normal";
-
-            let itemsBloc = document.createElement('ul');
-            itemsBloc.className = "row list-group-flush col-12 col-sm-12 text-center mx-auto ";
-
-            let itemName = document.createElement('li');
-            itemName.className = "text-left col-10 mx-auto list-group-item";
-
-            let itemPrice = document.createElement('li');
-            itemPrice.className = "text-right col-3 mr-auto list-group-item";
-
-            let itemDescription = document.createElement('li');
-            itemDescription.className = "text-left col-7 ml-auto list-group-item";
-
-            let itemPic = document.createElement('li');
-            itemPic.className = "text-center col-10 list-group-item mx-auto";
-
-            itemsLink.setAttribute("href",`produit.html?id=${itemsData[i]._id}`); // LIEN DU PRODUIT 
-            itemName.textContent = itemsData[i].name;
-            itemPrice.textContent = `${itemsData[i].price} €`;
-            itemDescription.textContent = itemsData[i].description;
-            itemPic.innerHTML = `<img src="${itemsData[i].imageUrl}" class="img-fluid">`;
-         
-            // LAYOUT            
-            mainBloc.appendChild(itemsLink);
-            itemsLink.appendChild(itemsBloc);
-            itemsBloc.appendChild(itemPic);
-            itemsBloc.appendChild(itemName);
-            itemsBloc.appendChild(itemDescription);
-            itemsBloc.appendChild(itemPrice);
-
-        }
-    }
-    }catch{
-        console.error();
-    }
-
-};
-fetchIndex();
+    let product =  `<a class = "badge badge-light col-md-6 col-sm-12 mx-auto" href = "produit.html?id=${item._id}" style = "white-space: normal;" > 
+                        <ul class = "row list-group-flush col-12 col-sm-12 text-center mx-auto "> 
+                            <li class = "text-center col-10 list-group-item mx-auto"> 
+                                <img src=${item.imageUrl} class="img-fluid"></li>
+                            <li class="text-left col-10 mx-auto list-group-item">${item.name}</li> 
+                            <li class = "text-left col-7 ml-auto list-group-item">${item.description}</li>
+                            <li class="text-right col-3 mr-auto list-group-item">${formatPrice(item.price)} €</li> 
+                        </ul>
+                    </a>`;
+    container.innerHTML += product; // CREATION D'UN BLOC 
+                                    // SUPPLEMENTAIRE A CHAQUE TOUR DE BOUCLE               
+}
+//-----------------------FONCTION : FORMATER LE PRIX
+export function formatPrice(price){
+    return (price/100).toFixed(2);
+}
